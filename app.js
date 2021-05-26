85af3087fe84829245fff6f5ada5ee237d752515
JamesTheron36 <80755975+JamesTheron36@users.noreply.github.com>
2021-05-26 09:27:31 +0200

    commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const NavSlider = () =>{
+	const burger = document.querySelector('.burger');
+	const nav = document.querySelector('.nav-links');
+	const navLinks = document.querySelectorAll('.nav-links li');
+
+	burger.addEventListener('click', () => {
+		nav.classList.toggle('nav-active')
+
+		navLinks.forEach((link, index) => {
+		if(link.style.animation){
+			link.style.animation = '';
+		}
+		else{
+			link.style.animation = `NavLinkFade 0.5s ease forwards ${index/7 + 0.8}s`
+		}
+		
+	});
+
+		//burger anim
+		burger.classList.toggle('toggle');
+	});
+
+
+} 
+
+
+const app = () =>{
+	NavSlider();
+}
+
+app();
