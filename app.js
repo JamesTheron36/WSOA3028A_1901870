f7134fd5ddc0745656b2c024764fc782abe88a3e
JamesTheron36 <80755975+JamesTheron36@users.noreply.github.com>
2021-07-02 15:05:05 +0200

    Updates

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+
+const hero = document.querySelector('.hero');
+const headline = document.querySelector('.headline');
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, {height: "0%"}, {height: "80%", ease: Power2.easInOut}).fromTo(hero, 1.2, {width: "100%"}, {width: "95%", ease: Power2.easInOut});
+
 const NavSlider = () =>{
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
@@ -28,7 +36,7 @@ const app = () =>{
 	NavSlider();
 }
 
-app();
+
 
 var x = document.getElementsByTagName("A");
 var links = Array.from(x)
@@ -47,3 +55,206 @@ function addSound(item) {
   })
 }
 
+
+
+const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?id=993800&units=metric&appid=53217561e22fa44ddb8df2b800ca3a59";
+const timeUrl = "http://worldtimeapi.org/api/timezone/Africa/Johannesburg"
+
+
+const currTime = document.getElementById('time');
+const maxTemp = document.getElementById('maxTemp');
+const minTemp = document.getElementById('minTemp');
+const currTemp = document.getElementById('temp');
+const humidity = document.getElementById('hum');
+const description = document.getElementById('desc');
+const icon = document.getElementById('weatherIcon');
+const weatherCard = document.getElementById('weatherCard');
+
+let sunrise = 0;
+let sunset = 0;
+let hum = 0;
+let temp = 0;
+let tempMin = 0;
+let tempMax = 0;
+let desc = "";
+let time = 0;
+let cond = "";
+let strTime = ""
+
+function FetchWeatherData(){
+	fetch(weatherUrl).then(res => {
+		if(res.ok){
+			return res.json();
+		}
+		else{
+			console.log("failure");
+		}
+	}).then(data =>{
+	//currTemp.innerHTML = Math.round(data.main.temp).toString() + "&deg;";
+	temp = Math.round(data.main.temp);
+	desc = data.weather[0].description;
+	cond = data.weather[0].main;
+	tempMin = Math.round(data.main.temp_min)
+	tempMax = Math.round(data.main.temp_max)
+	hum = Math.round(data.main.humidity)
+	//console.log(temp);
+	sunrise = data.sys.sunrise;
+	sunset = data.sys.sunset;
+	console.log(strTime + " time");
+	UpdateWeatherModule();
+	SetIcon(cond);
+	})
+			
+	
+		
+}
+
+function FetchTimeData(){
+	fetch(timeUrl).then(res => {
+		if(res.ok){
+			return res.json();
+		}
+		else{
+			console.log("failure");
+		}
+	}).then(data =>{
+	timeData = data;
+	time = data.unixtime;
+	strTime = ConvertTime(data.unixtime);
+	
+	
+	FetchWeatherData();
+	//UpdateTimeModule(time);
+	//SetIcon(cond);
+	})
+}
+
+
+function ConvertTimeMins(unix){
+	var date = new Date(unix*1000);
+	var mins = 0 + date.getMinutes();
+	return mins;
+}
+
+function ConvertTimeSeconds(unix){
+	var date = new Date(unix*1000);
+	var mins = 0 + date.getSeconds();
+	return mins;
+}
+
+function ConvertTimeHours(unix){
+	var date = new Date(unix*1000);
+	var hours = 0 + date.getHours();
+	return hours;
+}
+function ConvertTime(unix){
+	var date = new Date(unix*1000);
+	var hours = date.getHours();
+	var mins = date.getMinutes();
+	if(mins > 9){
+		var str = hours.toString() + ":" + mins.toString();
+		return str;
+	}
+	else{
+		var str = hours.toString() + ":0" + mins.toString();
+	return str;
+	}
+	
+}
+
+function UpdateWeatherModule(){
+	console.log(temp);
+	currTemp.innerHTML = temp.toString() + "&deg;";
+	description.innerHTML = desc;
+	maxTemp.innerHTML = tempMax.toString() + "&deg;";
+	minTemp.innerHTML = tempMin.toString() + "&deg;";
+	humidity.innerHTML = hum.toString() + "%";
+	currTime.innerHTML = strTime;
+}
+
+function SetIcon(condition){
+	
+	var night = false;
+	if(time > sunrise & time < sunset){
+		night = false;
+		weatherCard.style.background = "var(--background-grad-day)";
+	}
+	else if(time >= sunset){
+		night = true;
+		weatherCard.style.background = "var(--background-grad-night)";
+	}
+
+	switch(condition){
+		case "Thunderstorm":
+			if(night == true){
+				icon.src = "Icons/storm_night.png";
+				console.log(icon.src);
+			}
+			else{
+				icon.src = "Icons/storm_day.png";
+				console.log(icon.src);
+			}
+			break;
+		case "Drizzle":
+			if(night == true){
+				icon.src = "Icons/drizzle_night.png";
+				console.log(icon.src);
+			}
+			else{
+				icon.src = "Icons/drizzle_day.png";
+				console.log(icon.src);
+			}
+			break;
+		case "Clouds":
+			if(night == true){
+				icon.src = "Icons/night_cloudy.png";
+				console.log(icon.src);
+			}
+			else{
+				icon.src = "Icons/day_cloudy.png";
+				console.log(icon.src);
+			}
+			break;
+		case "Clear":
+			if(night == true){
+				icon.src = "Icons/night.png";
+				console.log(icon.src);
+			}
+			else{
+				icon.src = "Icons/sun.png";
+				console.log(icon.src);
+			}
+			break;
+		case "Rain": 
+			icon.src = "Icons/rainy.png";
+			console.log(icon.src);
+			break;
+		default:
+			if(night == true){
+				icon.src = "Icons/night.png";
+				console.log(icon.src);
+			}
+			else{
+				icon.src = "Icons/sun.png";
+				console.log(icon.src);
+			}
+			break;
+	}
+
+	
+
+}
+
+function UpdateTimeModule(str){
+	currTime.innerHTML = str;
+
+}
+
+
+
+
+app();
+FetchTimeData();
+setInterval(function(){ FetchTimeData(); }, 63000);
+
+//UpdateWeatherModule();
